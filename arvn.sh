54d46187ab146646ccbf034e5c3d102445ef1bb5
kandiyyas01 <53626533+kandiyyas01@users.noreply.github.com>
2020-05-17 14:10:56 +0700

    Add files via upload

---
+++ arvn.sh
@@ -0,0 +1,131 @@
+clear
+cyan='\e[0;36m'
+purple='\e[0;35m'
+brown='\e[0;33m'
+lightgray='\e[0;37m'
+darkgray='\e[0;30m'
+lightblue='\e[0;34m'
+lightgreen='\e[0;32m'
+lightcyan='\e[0;36m'
+lightred='\e[0;31m'
+lightpurple='\e[0;35m'
+yellow='\e[0;33m'
+white='\e[0;37m'
+toilet -f future ArvanArdiansyah | lolcat
+toilet -f pagga Welcome Kaka^-^ | lolcat
+echo "Tools Terbaru versi 2.1" | lolcat
+sleep 1
+echo "Jika Ada penyalahgunaan Kami tidak bertanggung jawab atas perilaku
+anda" | lolcat
+sleep 1
+echo "█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
+
+Author : ArvanArdian
+Kontak : 081252282115
+Pesan  : Install Bahan Dulu
+         Sebelum Mulai
+
+█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█" | lolcat
+
+echo "[+]Dipilih!" | lolcat
+echo "█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
+
+[1] TERMUX FAKEROOT
+
+[2] VIRUS TROJANS
+
+[3] HACK FACEBOOK
+
+[4] SPAM SMS
+
+[5] CEK IP SAYA
+
+[6] BOTCHAT/SIMSIMI
+
+[7] SCAN IP WEBSITE
+
+[8] TEMBAK KUOTA
+
+[9] INSTALL BAHAN
+
+[0] KELUAR
+
+█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█" |lolcat
+
+read -p "PILIH NOMOR DIATAS~[ " p
+
+if [ $p = 1 ] || [ $p = 1 ]
+then
+git clone https://github.com/saydog/termux-fakeroot.git
+cd termux-fakeroot
+chmod +x setup
+./setup
+fi
+
+if [ $p = 2 ] || [ $p = 2 ]
+then
+git clone https://github.com/Bl4ckDr460n/STrojans
+cd STrojans
+python2 STrojans.py
+fi
+
+if [ $p = 3 ] || [ $p = 3 ]
+then
+git clone https://github.com/Bl4ckDr460n/Black-Fb-Premium
+cd Black-Fb-Premium
+python2 Black-Fb.py
+fi
+
+if [ $p = 4 ] || [ $p = 4 ]
+then
+git clone https://github.com/Bl4ckDr460n/SPAMer
+cd SPAMer
+python2 SPAMer.py
+fi
+
+if [ $p = 5 ] || [ $p = 5 ]
+then
+git clone https://github.com/Bl4ckDr460n/My-Ip
+cd My-Ip
+python2 My-Ip.py
+fi
+
+if [ $p = 6 ] || [ $p = 6 ]
+then
+git clone https://github.com/Bl4ckDr460n/BotChat
+cd BotChat
+python2 BotChat.py
+fi
+
+if [ $p = 7 ] || [ $p = 7 ]
+then
+git clone https://github.com/Bl4ckDr460n/ScanIp
+cd ScanIp
+python2 scanip.py
+fi
+
+if [ $p = 8 ] || [ $p = 8 ]
+then
+git clone https://github.com/MrTamfanX/TembakKuota
+cd TembakKuota
+ls
+fi
+
+if [ $p = 9 ] || [ $p = 9 ]
+then
+sh i.sh
+fi
+
+if [ $p = 0 ] || [ $p = 0 ]
+then
+echo "Makasih Telah menggunakan Tools ini dengan Bijak^,^" | lolcat
+sleep 1
+echo " thank you for using tools this wisely^,^" | lolcat
+sleep 1
+echo "  Bye Bye" | lolcat
+sleep 1
+echo "   see you next time^,^" | lolcat
+sleep 1
+echo "[×] EXIT!" | lolcat
+exit
+fi
